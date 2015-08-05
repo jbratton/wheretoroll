1ac7c260c02248a333a5ccb61b404e730802c463
jbratton <jeremy@alumni.rice.edu>
2015-08-04 19:34:26 -0500

    fix 00xx years in form submissions, remove unused jbuilder code

--- app/controllers/derby_events_controller.rb
+++ app/controllers/derby_events_controller.rb
@@ -2,7 +2,6 @@ class DerbyEventsController < ApplicationController
   before_action :set_derby_event, only: [:show, :edit, :update, :destroy]
 
   # GET /derby_events
-  # GET /derby_events.json
   def index
     if admin_signed_in?
       @derby_events = DerbyEvent.all
@@ -12,7 +11,6 @@ class DerbyEventsController < ApplicationController
   end
 
   # GET /derby_events/1
-  # GET /derby_events/1.json
   def show
     if !admin_signed_in? && (@derby_event.deleted || !@derby_event.approved)
       redirect_to root_path
@@ -20,7 +18,6 @@ class DerbyEventsController < ApplicationController
     end
     respond_to do |format|
       format.html { render layout: false }
-      format.json
     end
   end
 
@@ -35,7 +32,6 @@ class DerbyEventsController < ApplicationController
   end
 
   # POST /derby_events
-  # POST /derby_events.json
   def create
     derby_event_params[:start_date]
     @derby_event = DerbyEvent.new(derby_event_params)
@@ -43,10 +39,8 @@ class DerbyEventsController < ApplicationController
     respond_to do |format|
       if @derby_event.save
         format.html { redirect_to thanks_path }
-        format.json { render :show, status: :created, location: @derby_event }
       else
         format.html { render :new }
-        format.json { render json: @derby_event.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -55,28 +49,23 @@ class DerbyEventsController < ApplicationController
   end
 
   # PATCH/PUT /derby_events/1
-  # PATCH/PUT /derby_events/1.json
   def update
     redirect_to root_path unless admin_signed_in?
     respond_to do |format|
       if @derby_event.update(derby_event_params)
         format.html { redirect_to root_path, notice: 'Event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @derby_event }
       else
         format.html { render :edit }
-        format.json { render json: @derby_event.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /derby_events/1
-  # DELETE /derby_events/1.json
   def destroy
     redirect_to root_path unless admin_signed_in?
     @derby_event.destroy
     respond_to do |format|
       format.html { redirect_to derby_events_url, notice: 'Derby event was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -88,6 +77,8 @@ class DerbyEventsController < ApplicationController
 
     def format_date_for_db(form_date)
       month, day, year = form_date.split('/')
+      year = year[-2, 2] if year[0, 2] == "00"
+      year = "20" << year[-2, 2] if year.length == 2
       "#{year}-#{month}-#{day}"
     end
 
--- app/views/derby_events/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@derby_events) do |derby_event|
-  json.extract! derby_event, :id, :name, :start_date, :end_date, :venue, :city, :state, :zip_code, :country, :web_site, :contact, :host, :cost, :ruleset, :preregistration, :spectators
-  json.url derby_event_url(derby_event, format: :json)
-end
